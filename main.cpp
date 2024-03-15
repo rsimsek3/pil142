1cd597702bc2485974449c21021675a2bcbb9cc1
ofarukinciler <163448266+ofarukinciler@users.noreply.github.com>
2024-03-15 22:49:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,48 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
+using namespace std;
 
 class Gemi {
 public:
-    int yakit;
-    int saglik;
-    int para;
+    int saglik = 100;
+    int yakit = 100;
+    int para = 0;
+    virtual float kacmaOlasiligi() = 0;
+    virtual float hasarKatsayisi() = 0;
 };
 
 class HizliGemi : public Gemi {
 public:
-    double hasar_carpici;
-    double kacma_olasiligi;
-    HizliGemi() {
-        yakit = 100;
-        saglik = 100;
-        para = 0;
-        hasar_carpici = 1.5;
-        kacma_olasiligi = 1.5;
+    float kacmaOlasiligi() override {
+        return 1.5;
     }
 
-
+    float hasarKatsayisi() override {
+        return 1.5;
+    }
 };
+
 class GucluGemi : public Gemi {
 public:
-    double hasar_carpici;
-    double kacma_olasiligi;
-    GucluGemi() {
-        yakit = 100;
-        saglik = 100;
-        para = 0;
-        hasar_carpici = 0.5;
-        kacma_olasiligi = 0.5;
+    float kacmaOlasiligi() override {
+        return 0.5;
     }
 
-
+    float hasarKatsayisi() override {
+        return 0.5;
+    }
 };
 
 class NormalGemi : public Gemi {
 public:
-    double hasar_carpici;
-    double kacma_olasiligi;
-    NormalGemi() {
-        yakit = 100;
-        saglik = 100;
-        para = 0;
-        hasar_carpici = 1.0;
-        kacma_olasiligi = 1.0;
+    float kacmaOlasiligi() override {
+        return 1.0;
     }
 
+    float hasarKatsayisi() override {
+        return 1.0;
+    }
 };
