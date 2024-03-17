e9f2e429abcf115e31f98fc63a755525233c50f0
rsimsek3 <163339357+rsimsek3@users.noreply.github.com>
2024-03-17 22:52:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -63,3 +63,59 @@ void TerkedilmisGezegen(Gemi& Gemi) {
         cout << "EYVAHH!!! Geminiz hasar aldi. Guncel gemi sagligi: " << Gemi.saglik << endl;
     }
 }
+void uzayKorsanlari(Gemi& gemi) {
+    cout << "Uzay korsanlari saldiriyor! Arkani kolla " << endl;
+    int secim, istenenAltin;
+    bool gecerliSecim = false;
+
+    while (!gecerliSecim) {
+        cout << "1. Kac (33 yakit harcar)\n2. Savas\n3. Pazarlik";
+        cout << "\nSeciminizi girin: ";
+        cin >> secim;
+
+        switch (secim) {
+            case 1: // Kaçma seçimi
+                gecerliSecim = true;
+                if (gemi.yakit < 33) {
+                    cout << "Kacmak icin yeterli yakitiniz yok!" << endl;
+                    gecerliSecim = false; //  yeniden seçim yaptırmak için
+                } else {
+                    gemi.yakit -= 33;
+                    if (rand() % 100 < 50 * gemi.kacmaOlasiligi()) {
+                        cout << "Basariyla kactiniz!" << endl;
+                    } else {
+                        cout << "Kacamadiniz ve naneyi yediniz. Korsanlar hala pesinizde!" << endl;
+                    }
+                }
+                break;
+            case 2: // Savaş seçimi
+                gecerliSecim = true;
+                if (rand() % 2) {
+                    cout << "Korsanlarin icinden gectiniz!!!" << endl;
+                } else {
+                    gemi.saglik -= 30 * gemi.hasarKatsayisi();
+                    cout << "Savasi ve baska bir seyi  kaybettiniz; ayrica hasar aldiniz. Guncel saglik: " << gemi.saglik << endl;
+                }
+                break;
+            case 3: // Pazarlık
+                istenenAltin = (rand() % 3 + 1) * 10; // Korsanların istediği altın miktarı
+                cout << "Korsanlar sizden " << istenenAltin << " altin istiyor." << endl;
+                cout << "Korsanlar Beyi: Ya parani ya da ...'nu alirim yegenim " << endl;
+                if (istenenAltin <= gemi.para) {
+                    gemi.para -= istenenAltin;
+                    gecerliSecim = true;
+                    cout << "Korsanlar Beyi: Guzel boyle adam ol, gelecekte vermeye devam " << endl;
+
+                } else {
+                    cout << "Korsanlara odeyecek kadar altininiz yok." << endl;
+                    cout << "Ya savasmali ya da kacmalisiniz." << endl;
+                    // yeniden seçime zorlar ve tekrar pazarlık seçse de yeniden seçime zorlar.
+                }
+                break;
+            default:
+                cout << "Gecersiz secim. Tekrar deneyin." << endl;
+                // bu yazıyı görmek isteyen  1 2 3 dışında bir sayı yazsın sürekli
+        }
+    }
+}
+
